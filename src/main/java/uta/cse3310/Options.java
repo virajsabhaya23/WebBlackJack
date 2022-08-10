8a0f0a67969e8a022e90d583c29f3725fc8c716f
Obinna Nwokocha <obiboy19@gmail.com>
2022-08-10 17:56:32 -0500

    Add files via upload

--- src/main/java/uta/cse3310/Options.java
+++ src/main/java/uta/cse3310/Options.java
@@ -2,25 +2,29 @@
 package uta.cse3310;
 
 import uta.cse3310.Game.*;
-import uta.cse3310.Game.Check;
 
 public class Options {
-    public static void hit(Dealer dealer, Player player) {
+    public static void hit(Dealer dealer, Player player) 
+    {
         dealer.dealCard(player.hand);
         player.round = false;
     }
 
-    public static void doubledown(Dealer dealer, Player player) {
-        player.round = false;
+    public static void doubledown(Dealer dealer, Player player) 
+    {
+        player.placeBet(player.bet);
         dealer.dealCard(player.hand);
-        Check.doubled(dealer, player);
-    }
-
-    public static void stand(Player player) {
         player.round = false;
     }
 
-    public static void split(Player player) {
+    public static void stand(Player player) 
+    {
+        player.round = false;
+    }
+
+    public static void split(Player player) 
+    {
+        
         player.round = false;
     }
 }
--- src/main/java/uta/cse3310/WebBlackJack.java
+++ src/main/java/uta/cse3310/WebBlackJack.java
@@ -1,93 +1,100 @@
 package uta.cse3310;
 
 import java.util.*;
-import java.util.ArrayList;
-import java.lang.*;
-
-import uta.cse3310.Game.Card;
-import uta.cse3310.Game.Hand;
 import uta.cse3310.Game.Dealer;
-import uta.cse3310.Game.Suite;
-import uta.cse3310.Game.Num;
 import uta.cse3310.Game.Player;
 import uta.cse3310.Game.Check;
 
+/*import java.util.ArrayList;
+import java.lang.*;
+import uta.cse3310.Game.Suite;
+import uta.cse3310.Game.Num;
 import com.google.gson.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.JsonParseException;*/
 
 public class WebBlackJack {
-
-        public static void main(String[] args) {
-
+        public static void main(String[] args) 
+        {
                 final int num = 1;
                 int in;
                 boolean play = true;
-                Dealer dealer = new Dealer("Dealer", num);
-                dealer.initializeCards();
-                Player player = new Player("P1");
+                Scanner scn = new Scanner(System.in);
 
-                dealer.dealCard(dealer.hand);
-                while (play == true) {
-                        dealer.shuffle(player);
+                //Gson gson = new Gson();
+                //String json; //= gson.toJson(deck.hand.get(i))
+                while (play == true) 
+                {
+                        Dealer dealer = new Dealer("Dealer", num);
+                        Player player = new Player("P1");
 
-                        for (int j = 0; j < 2; j++) {
+                        System.out.print("How much money do you have: ");
+                        player.balance = scn.nextInt();
+
+                        System.out.print("How much do you want to bet: ");
+                        player.placeBet(scn.nextInt());
+
+                        dealer.initializeCards();
+                        dealer.dealCard(dealer.hand);
+
+                        for (int j = 0; j < 2; j++) 
+                        {
                                 dealer.dealCard(player.hand);
-
                         }
 
-                        Scanner scn = new Scanner(System.in);
-
-                        while (player.busted == false) {
-
+                        while (player.busted == false) 
+                        {
                                 player.round = true;
-
                                 Check.count(player, dealer);
 
-                                while (player.round == true) {
+                                while (player.round == true) 
+                                {
                                         Check.print_cards(dealer, player);
-                                        // System.out.println("\n////////////////////////////////////////////\n");
-                                        System.out.println("1) Hit\n2) Stand\n3) DoubleDown\n4) Split");
+                                        System.out.println("Player Balance: " + player.balance + "\nPlayer Bet: " + player.bet + "\n\n");
+                                        System.out.println("1) Hit\n2) Stand\n3) DoubleDown\n4) Split\n");
                                         in = scn.nextInt();
 
-                                        if (in == 1) {
-                                                Options.hit(dealer, player);
-                                                Check.count(player, dealer);
-                                                Check.check_val(dealer, player);
-                                        } else if (in == 2) {
-                                                Options.stand(player);
-                                                Check.count(player, dealer);
-                                                Check.check_val(dealer, player);
-                                        } else if (in == 3) {
-                                                Options.doubledown(dealer, player);
-                                        } else if (in == 4) {
-                                                Options.split(player);
-                                                Check.count(player, dealer);
-                                                Check.check_val(dealer, player);
+                                        switch(in)
+                                        {
+                                                case 1: Options.hit(dealer, player);
+                                                        break;
+                                                case 2: Options.stand(player);
+                                                        break;
+                                                case 3: Options.doubledown(dealer, player);
+                                                        break;
+                                                case 4: Options.split(player);
+                                                        break;
+                                                default:System.out.println("Default option - Hit"); 
+                                                        Options.hit(dealer, player);
+                                                        break;
                                         }
+                                        Check.count(player, dealer);
+                                        System.out.println("\n\nPlayer Balance: " + player.balance + "\nPlayer Bet: " + player.bet + "\n\n");
+                                        Check.check_val(dealer, player);
 
-                                        // Gson gson = new Gson();
-                                        // String json = gson.toJson(deck.hand.get(i));
-                                        // String data = "{'suite': 'Hearts','number': 'Queen','value': 10 }";
-                                        // System.out.print(data);
-                                        // Gson.fromJson(data, Card.class);
-                                }
-                                System.out.println("Continue?\n1.YES\n2.NO\n");
-                                in = scn.nextInt();
-                                if (in == 1) {
-                                        player.busted = false;
+                                        if(in == 3 && player.value < 21)
+                                        {
+                                                System.out.println("You Lose");
+                                                player.busted = true;
+                                        }
                                 }
 
                         }
                         System.out.println("\n1) Continue?\n2) Quit");
                         in = scn.nextInt();
-                        if (in == 2) {
+                        if(in == 1)
+                        {
+                                player.busted = false;
+                        }
+                        else if(in == 2) 
+                        {
                                 play = false;
                         }
                 }
+                scn.close();       
         }
 }
